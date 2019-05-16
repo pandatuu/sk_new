3b573a4c4a2723be70f179c1e397628718de60b1
LIZHENCHUAN <395387944@qq.com>
2019-05-16 17:14:44 +0800

    [Add]Add onlineresume/AddEduExperience page

---
+++ app/src/main/java/com/example/sk_android/mvp/view/fragment/onlineresume/AddEduExperienceFrag.kt
@@ -0,0 +1,256 @@
+package com.example.sk_android.mvp.view.fragment.onlineresume
+
+import android.content.Context
+import android.graphics.Color
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.codbking.widget.DatePickDialog
+import com.codbking.widget.bean.DateType
+import com.example.sk_android.R
+import org.jetbrains.anko.*
+import org.jetbrains.anko.support.v4.UI
+
+
+class AddEduExperienceFrag : Fragment() {
+
+    lateinit var mContext: Context
+
+    companion object {
+        fun newInstance(context: Context): AddEduExperienceFrag {
+            val fragment = AddEduExperienceFrag()
+            fragment.mContext = context
+            return fragment
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var fragmentView = createView()
+
+        return fragmentView
+    }
+
+    private fun createView(): View? {
+        return UI {
+            linearLayout {
+                scrollView {
+                    verticalLayout {
+                        // 学校名
+                        relativeLayout {
+                            backgroundResource = R.drawable.text_view_bottom_border
+                            textView {
+                                text = "学校名"
+                                textSize = 14f
+                                textColor = Color.parseColor("#FF999999")
+                            }.lparams {
+                                width = wrapContent
+                                height = wrapContent
+                                topMargin = dip(15)
+                            }
+                            textView {
+                                text = "学校名を入力してください"
+                                textSize = 17f
+                                textColor = Color.parseColor("#FF333333")
+                            }.lparams {
+                                width = wrapContent
+                                height = wrapContent
+                                topMargin = dip(45)
+                            }
+                        }.lparams {
+                            width = matchParent
+                            height = dip(85)
+                            leftMargin = dip(15)
+                            rightMargin = dip(15)
+                        }
+                        // 学歴
+                        relativeLayout {
+                            backgroundResource = R.drawable.text_view_bottom_border
+                            textView {
+                                text = "学歴"
+                                textSize = 14f
+                                textColor = Color.parseColor("#FF999999")
+                            }.lparams {
+                                width = wrapContent
+                                height = wrapContent
+                                topMargin = dip(15)
+                            }
+                            relativeLayout {
+                                var textv = textView {
+                                    text = "学歴を選択してください"
+                                    textSize = 17f
+                                    textColor = Color.parseColor("#FF333333")
+                                }.lparams {
+                                    width = wrapContent
+                                    height = wrapContent
+                                    topMargin = dip(15)
+                                    centerVertically()
+                                }
+                                var tool = toolbar {
+                                    navigationIconResource = R.mipmap.icon_go_position
+                                }.lparams {
+                                    width = dip(22)
+                                    height = dip(22)
+                                    alignParentRight()
+                                    centerVertically()
+                                }
+                            }.lparams {
+                                width = wrapContent
+                                height = matchParent
+                                topMargin = dip(25)
+                            }
+                        }.lparams {
+                            width = matchParent
+                            height = dip(80)
+                            leftMargin = dip(15)
+                            rightMargin = dip(15)
+                        }
+                        // 専門科目
+                        relativeLayout {
+                            backgroundResource = R.drawable.text_view_bottom_border
+                            textView {
+                                text = "専門科目"
+                                textSize = 14f
+                                textColor = Color.parseColor("#FF999999")
+                            }.lparams {
+                                width = wrapContent
+                                height = wrapContent
+                                topMargin = dip(15)
+                            }
+                            var textv = textView {
+                                text = "専門科目を入力してください"
+                                textSize = 17f
+                                textColor = Color.parseColor("#FF333333")
+                            }.lparams {
+                                width = wrapContent
+                                height = wrapContent
+                                topMargin = dip(45)
+                            }
+                        }.lparams {
+                            width = matchParent
+                            height = dip(85)
+                            leftMargin = dip(15)
+                            rightMargin = dip(15)
+                        }
+                        // 開始時間
+                        relativeLayout {
+                            backgroundResource = R.drawable.text_view_bottom_border
+                            textView {
+                                text = "開始時間"
+                                textSize = 14f
+                                textColor = Color.parseColor("#FF999999")
+                            }.lparams {
+                                width = wrapContent
+                                height = wrapContent
+                                topMargin = dip(15)
+                            }
+                            relativeLayout {
+                                var textv = textView {
+                                    text = "開始時間を選択する"
+                                    textSize = 17f
+                                    textColor = Color.parseColor("#FF333333")
+                                }.lparams {
+                                    width = wrapContent
+                                    height = wrapContent
+                                    topMargin = dip(15)
+                                    centerVertically()
+                                }
+                                var tool = toolbar {
+                                    navigationIconResource = R.mipmap.icon_go_position
+                                }.lparams {
+                                    width = dip(22)
+                                    height = dip(22)
+                                    alignParentRight()
+                                    centerVertically()
+                                }
+                            }.lparams {
+                                width = wrapContent
+                                height = matchParent
+                                topMargin = dip(25)
+                            }
+                        }.lparams {
+                            width = matchParent
+                            height = dip(80)
+                            leftMargin = dip(15)
+                            rightMargin = dip(15)
+                        }
+                        // 終了時間
+                        relativeLayout {
+                            backgroundResource = R.drawable.text_view_bottom_border
+                            textView {
+                                text = "終了時間"
+                                textSize = 14f
+                                textColor = Color.parseColor("#FF999999")
+                            }.lparams {
+                                width = wrapContent
+                                height = wrapContent
+                                topMargin = dip(15)
+                            }
+                            relativeLayout {
+                                var textv = textView {
+                                    text = "終了時間を選択する"
+                                    textSize = 17f
+                                    textColor = Color.parseColor("#FF333333")
+                                }.lparams {
+                                    width = wrapContent
+                                    height = wrapContent
+                                    topMargin = dip(15)
+                                    centerVertically()
+                                }
+                                var tool = toolbar {
+                                    navigationIconResource = R.mipmap.icon_go_position
+                                }.lparams {
+                                    width = dip(22)
+                                    height = dip(22)
+                                    alignParentRight()
+                                    centerVertically()
+                                }
+                            }.lparams {
+                                width = wrapContent
+                                height = matchParent
+                                topMargin = dip(25)
+                            }
+                        }.lparams {
+                            width = matchParent
+                            height = dip(80)
+                            leftMargin = dip(15)
+                            rightMargin = dip(15)
+                        }
+                        // 獲得した賞
+                        relativeLayout {
+                            textView {
+                                text = "獲得した賞"
+                                textSize = 14f
+                                textColor = Color.parseColor("#FF999999")
+                            }.lparams {
+                                width = wrapContent
+                                height = wrapContent
+                                topMargin = dip(15)
+                            }
+                            editText {
+                                backgroundResource = R.drawable.area_text
+                                gravity = top
+                            }.lparams {
+                                width = matchParent
+                                height = dip(170)
+                                topMargin = dip(45)
+                            }
+                        }.lparams {
+                            width = matchParent
+                            height = dip(220)
+                            leftMargin = dip(15)
+                            rightMargin = dip(15)
+                        }
+                    }.lparams {
+                        width = matchParent
+                        height = matchParent
+                    }
+                }.lparams {
+                    width = matchParent
+                    height = matchParent
+                }
+            }
+        }.view
+    }
+}
